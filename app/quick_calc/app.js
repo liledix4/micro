3b1c7fdb72f33e1332f6a03a8001d19991d9c9c7
liledix4 <liledix4@musician.org>
2025-01-19 02:11:41 +0000

    [2025.01.19]

--- app/quick_calc/app.js
+++ app/quick_calc/app.js
@@ -4,19 +4,21 @@ const result = document.getElementById('result');
 
 function doIt() {
     function parse(str) {
-        str = str.toString();
-        if (str.replace(',', '.').match(/^\s*[0-9\.\(]?[0-9\s\.\+\-\*\/\^\(\)%]*[0-9\s\.\)]$/)) {
+        str = str.toString()
+            .replaceAll(/[^0-9\s\.\+\-\*\/\^()%]/g,'')
+            .replace(',', '.')
+            .replace(/^[^0-9\.(]+|[^0-9\.)]+$/,'');
+        if (str === '')
+            return 0;
+        else if (str.match(/^[0-9\s\.\+\-\*\/\^()%]*$/)) {
             try {
                 return eval(`"use strict"; (${str})`);
             }
             catch (error) {
+                console.error(error);
                 return 'Your expression has errors.';
             };
         }
-        else if (str.match(/[^0-9\s\.\+\-\*\/\^\(\)%]/))
-            return 'Forbidden symbols.';
-        else if (str === '')
-            return 0;
         else
             return '?';
     }
