859cccfd5c69328caa6dca59109f996d041f9253
liledix4 <liledix4@musician.org>
2025-02-02 20:26:45 +0000

    0.2.9

--- app/xhr_browser/app.js
+++ app/xhr_browser/app.js
@@ -17,6 +17,8 @@ function rejected() {
 }
 selector.input.addEventListener('submit', (event) => {
   event.preventDefault();
+  selector.status.innerText = '___';
+
   let url = selector.inputURL.value;
   const method = selector.inputMethod.value;
   const methodQuery = selector.inputMethodQuery.value;
@@ -28,7 +30,7 @@ selector.input.addEventListener('submit', (event) => {
     return;
   }
 
-  if (!url.match(/^https:\/\// && !url.match(/^\w:\/\//)))
+  if (!url.match(/^https:\/\//) && !url.match(/^\w+:\/\//))
     url = 'https://' + url;
   xhrObject.url = url;
 
@@ -38,18 +40,30 @@ selector.input.addEventListener('submit', (event) => {
   if (methodQuery !== '')
     send = methodQuery;
 
-  readTextFile(
-    xhrObject,
-    result => {
-      if (typeof result === 'object') {
-        selector.status.innerText = result.status;
-        selector.result.innerText = result.response;
-      }
-      else {
-        selector.status.innerText = '200';
-        selector.result.innerText = result;
-      }
-    },
-    send
-  );
+  try {
+    readTextFile(
+      xhrObject,
+      result => {
+        console.log(result);
+        if (typeof result === 'object') {
+          if (result.status === 0) {
+            selector.status.innerText = '???';
+            selector.result.innerText = 'Error.\nEither your URL is incorrect or resource is unavailable due to CORS policy.\nSome web servers may reject XHR requests.\n\nPress F12 or Ctrl+Shift+I to see more details.';
+          }
+          else {
+            selector.status.innerText = result.status;
+            selector.result.innerText = result.response;
+          }
+        }
+        else {
+          selector.status.innerText = '200';
+          selector.result.innerText = result;
+        }
+      },
+      send
+    );
+  } catch (error) {
+    selector.status.innerText = 'ERR';
+    selector.result.innerText = error;
+  }
 });
