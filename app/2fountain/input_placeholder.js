de3de208e71124b34b88e59681e727d9aa72bba5
liledix4 <liledix4@musician.org>
2025-02-03 05:00:15 +0000

    📱 Quick editing

--- app/2fountain/input_placeholder.js
+++ app/2fountain/input_placeholder.js
@@ -2,5 +2,5 @@ import { selector } from "./selectors.js";
 
 
 export function setPlaceholder() {
-  selector.input.setAttribute('placeholder', 'FEATURING:\nC1: CHARACTER #1\nC2: CHARACTER #2\n\nC1: Hi!\nC2: Ayo, what\'s up?\n\n@C1 takes time to think.\n\nC1: (with excitement) A ceiling!');
+  selector.input.setAttribute('placeholder', ':Featuring:\nC1: CHARACTER #1\nC2: CHARACTER #2\n\nC1: Hi!\nC2: Ayo, what\'s up?\n\n@C1 takes time to think.\n\nC1: (with excitement) A ceiling!');
 }
